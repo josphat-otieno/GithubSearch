5dc6c65dbc1567c3a9ade077f14ae49b249c7920
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-05-23 11:29:56 +0300

    created component myProfile and class profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyProfileComponent
   ],
   imports: [
     BrowserModule,
