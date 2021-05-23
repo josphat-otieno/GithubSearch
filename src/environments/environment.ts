8d9ee547db2a88c2f0e1f30acd9aaaaf94bf82fd
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-05-23 11:41:23 +0300

    generated apiKey and included environment.ts in .gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'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'
 };
 
 /*
