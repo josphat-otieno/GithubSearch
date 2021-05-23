44aeeaa2a2b3eb8dfaebca5237ac259c893d8e8d
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-05-23 11:33:48 +0300

    created class user and navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
+import { UserComponent } from './user/user.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyProfileComponent
+    MyProfileComponent,
+    UserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
