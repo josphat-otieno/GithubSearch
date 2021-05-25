7181425c352f14f2319dcd79b8e0d2ae982655f7
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-05-25 08:00:29 +0300

    add routing structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: MyProfileComponent},
+  {path: 'my-profile', component:MyProfileComponent},
+  {path: 'search', component:SearchFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
